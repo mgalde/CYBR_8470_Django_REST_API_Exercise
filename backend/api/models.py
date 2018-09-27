e178c98e10bf1f3af5db8038a9c183767ca79afb
Michael <30883926+mgalde@users.noreply.github.com>
2018-09-27 17:49:36 -0500

    added dog

--- backend/api/models.py
+++ backend/api/models.py
@@ -9,39 +9,39 @@ from django.contrib import admin
 import base64
 
 class Event(models.Model):
-    eventtype = models.CharField(max_length=1000, blank=False)
-    timestamp = models.DateTimeField()
-    userid = models.CharField(max_length=1000, blank=True)
-    requestor = models.GenericIPAddressField(blank=False)
+	eventtype = models.CharField(max_length=1000, blank=False)
+	timestamp = models.DateTimeField()
+	userid = models.CharField(max_length=1000, blank=True)
+	requestor = models.GenericIPAddressField(blank=False)
 
-    def __str__(self):
-        return str(self.eventtype)
+	def __str__(self):
+		return str(self.eventtype)
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('eventtype', 'timestamp')
+	list_display = ('eventtype', 'timestamp')
 
 class ApiKey(models.Model):
-    owner = models.CharField(max_length=1000, blank=False)
-    key = models.CharField(max_length=5000, blank=False)
+	owner = models.CharField(max_length=1000, blank=False)
+	key = models.CharField(max_length=5000, blank=False)
 
-    def __str__(self):
-        return str(self.owner) + str(self.key)
+	def __str__(self):
+		return str(self.owner) + str(self.key)
 
 class ApiKeyAdmin(admin.ModelAdmin):
-    list_display = ('owner','key')
+	list_display = ('owner','key')
 
 class DogList(APIView):
-    """
-    List all dogs, or create a new dog.
-    """
-    def get(self, request, format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
+	"""
+	List all dogs, or create a new dog.
+	"""
+	def get(self, request, format=None):
+		snippets = Snippet.objects.all()
+		serializer = SnippetSerializer(snippets, many=True)
+		return Response(serializer.data)
 
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	def post(self, request, format=None):
+		serializer = SnippetSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
