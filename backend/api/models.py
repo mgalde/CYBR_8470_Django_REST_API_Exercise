68adffa6160260e26b31e271d18a3b6ab8794974
Michael <30883926+mgalde@users.noreply.github.com>
2018-10-02 13:22:56 -0500

    Test Case

--- backend/api/models.py
+++ backend/api/models.py
@@ -50,8 +50,3 @@ class ApiKeyAdmin(admin.ModelAdmin):
 myapp.serializers import UserSerializer
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
-
-class UserList(generics.ListCreateAPIView):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer
-	permission_classes = (IsAdminUser,)
