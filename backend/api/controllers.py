aa77cb3c77d4e15744ddf2db7d799e1d79e0bd7b
Michael <30883926+mgalde@users.noreply.github.com>
2018-10-02 13:28:23 -0500

    Test Case

--- backend/api/controllers.py
+++ backend/api/controllers.py
@@ -1,5 +1,8 @@
 #from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from myapp.serializers import UserSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
 # Create your views here.
 from django.contrib.auth.models import *
 from django.contrib.auth import *
