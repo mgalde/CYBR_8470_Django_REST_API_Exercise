ba8fb5d9ce5cc924197e240871bf90df60193929
Michael <30883926+mgalde@users.noreply.github.com>
2018-10-02 12:26:29 -0500

    Adding in model

--- backend/api/controllers.py
+++ backend/api/controllers.py
@@ -40,6 +40,13 @@ import json, datetime, pytz
 from django.core import serializers
 import requests
 
+#More Filters to import
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 def home(request):
    """
@@ -155,6 +162,34 @@ class Events(APIView):
 		newEvent.save()
 		print 'New Event Logged from: ' + requestor
 		return Response({'success': True}, status=status.HTTP_200_OK)
+
+class SnippetDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class Dogs(APIView):
 	permission_classes = (AllowAny,)
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
     url(r'^dogs', csrf_exempt(controllers.Events.as_view())),
     url(r'^activateifttt', csrf_exempt(controllers.ActivateIFTTT.as_view())),
     url(r'^', include(router.urls)),
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
