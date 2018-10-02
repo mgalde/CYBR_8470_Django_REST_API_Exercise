56a9cf456be658a2fe9dc54177b692a8f4df2036
Michael <30883926+mgalde@users.noreply.github.com>
2018-10-02 10:41:53 -0500

    Space Conversion in controller.py

--- backend/api/controllers.py
+++ backend/api/controllers.py
@@ -46,160 +46,160 @@ def home(request):
    Send requests to / to the ember.js clientside app
    """
    return render_to_response('ember/index.html',
-               {}, RequestContext(request))
+			   {}, RequestContext(request))
 
 def xss_example(request):
   """
   Send requests to xss-example/ to the insecure client app
   """
   return render_to_response('dumb-test-app/index.html',
-              {}, RequestContext(request))
+			  {}, RequestContext(request))
 
 class Register(APIView):
-    permission_classes = (AllowAny,)
+	permission_classes = (AllowAny,)
 
-    def post(self, request, *args, **kwargs):
-        # Login
-        username = request.POST.get('username') #you need to apply validators to these
-        print username
-        password = request.POST.get('password') #you need to apply validators to these
-        email = request.POST.get('email') #you need to apply validators to these
-        gender = request.POST.get('gender') #you need to apply validators to these
-        age = request.POST.get('age') #you need to apply validators to these
-        educationlevel = request.POST.get('educationlevel') #you need to apply validators to these
-        city = request.POST.get('city') #you need to apply validators to these
-        state = request.POST.get('state') #you need to apply validators to these
+	def post(self, request, *args, **kwargs):
+		# Login
+		username = request.POST.get('username') #you need to apply validators to these
+		print username
+		password = request.POST.get('password') #you need to apply validators to these
+		email = request.POST.get('email') #you need to apply validators to these
+		gender = request.POST.get('gender') #you need to apply validators to these
+		age = request.POST.get('age') #you need to apply validators to these
+		educationlevel = request.POST.get('educationlevel') #you need to apply validators to these
+		city = request.POST.get('city') #you need to apply validators to these
+		state = request.POST.get('state') #you need to apply validators to these
 
-        print request.POST.get('username')
-        if User.objects.filter(username=username).exists():
-            return Response({'username': 'Username is taken.', 'status': 'error'})
-        elif User.objects.filter(email=email).exists():
-            return Response({'email': 'Email is taken.', 'status': 'error'})
+		print request.POST.get('username')
+		if User.objects.filter(username=username).exists():
+			return Response({'username': 'Username is taken.', 'status': 'error'})
+		elif User.objects.filter(email=email).exists():
+			return Response({'email': 'Email is taken.', 'status': 'error'})
 
-        #especially before you pass them in here
-        newuser = User.objects.create_user(email=email, username=username, password=password)
-        newprofile = Profile(user=newuser, gender=gender, age=age, educationlevel=educationlevel, city=city, state=state)
-        newprofile.save()
+		#especially before you pass them in here
+		newuser = User.objects.create_user(email=email, username=username, password=password)
+		newprofile = Profile(user=newuser, gender=gender, age=age, educationlevel=educationlevel, city=city, state=state)
+		newprofile.save()
 
-        return Response({'status': 'success', 'userid': newuser.id, 'profile': newprofile.id})
+		return Response({'status': 'success', 'userid': newuser.id, 'profile': newprofile.id})
 
 class Session(APIView):
-    permission_classes = (AllowAny,)
-    def form_response(self, isauthenticated, userid, username, error=""):
-        data = {
-            'isauthenticated': isauthenticated,
-            'userid': userid,
-            'username': username
-        }
-        if error:
-            data['message'] = error
+	permission_classes = (AllowAny,)
+	def form_response(self, isauthenticated, userid, username, error=""):
+		data = {
+			'isauthenticated': isauthenticated,
+			'userid': userid,
+			'username': username
+		}
+		if error:
+			data['message'] = error
 
-        return Response(data)
+		return Response(data)
 
-    def get(self, request, *args, **kwargs):
-        # Get the current user
-        if request.user.is_authenticated():
-            return self.form_response(True, request.user.id, request.user.username)
-        return self.form_response(False, None, None)
+	def get(self, request, *args, **kwargs):
+		# Get the current user
+		if request.user.is_authenticated():
+			return self.form_response(True, request.user.id, request.user.username)
+		return self.form_response(False, None, None)
 
-    def post(self, request, *args, **kwargs):
-        # Login
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return self.form_response(True, user.id, user.username)
-            return self.form_response(False, None, None, "Account is suspended")
-        return self.form_response(False, None, None, "Invalid username or password")
+	def post(self, request, *args, **kwargs):
+		# Login
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return self.form_response(True, user.id, user.username)
+			return self.form_response(False, None, None, "Account is suspended")
+		return self.form_response(False, None, None, "Invalid username or password")
 
-    def delete(self, request, *args, **kwargs):
-        # Logout
-        logout(request)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+	def delete(self, request, *args, **kwargs):
+		# Logout
+		logout(request)
+		return Response(status=status.HTTP_204_NO_CONTENT)
 
 class Events(APIView):
-    permission_classes = (AllowAny,)
-    parser_classes = (parsers.JSONParser,parsers.FormParser)
-    renderer_classes = (renderers.JSONRenderer, )
+	permission_classes = (AllowAny,)
+	parser_classes = (parsers.JSONParser,parsers.FormParser)
+	renderer_classes = (renderers.JSONRenderer, )
 
-    def get(self, request, format=None):
-        events = Event.objects.all()
-        json_data = serializers.serialize('json', events)
-        content = {'events': json_data}
-        return HttpResponse(json_data, content_type='json')
+	def get(self, request, format=None):
+		events = Event.objects.all()
+		json_data = serializers.serialize('json', events)
+		content = {'events': json_data}
+		return HttpResponse(json_data, content_type='json')
 
-    def post(self, request, *args, **kwargs):
-        print 'REQUEST DATA'
-        print str(request.data)
+	def post(self, request, *args, **kwargs):
+		print 'REQUEST DATA'
+		print str(request.data)
 
-        eventtype = request.data.get('eventtype')
-        timestamp = int(request.data.get('timestamp'))
-        userid = request.data.get('userid')
-        requestor = request.META['REMOTE_ADDR']
+		eventtype = request.data.get('eventtype')
+		timestamp = int(request.data.get('timestamp'))
+		userid = request.data.get('userid')
+		requestor = request.META['REMOTE_ADDR']
 
-        newEvent = Event(
-            eventtype=eventtype,
-            timestamp=datetime.datetime.fromtimestamp(timestamp/1000, pytz.utc),
-            userid=userid,
-            requestor=requestor
-        )
+		newEvent = Event(
+			eventtype=eventtype,
+			timestamp=datetime.datetime.fromtimestamp(timestamp/1000, pytz.utc),
+			userid=userid,
+			requestor=requestor
+		)
 
-        try:
-            newEvent.clean_fields()
-        except ValidationError as e:
-            print e
-            return Response({'success':False, 'error':e}, status=status.HTTP_400_BAD_REQUEST)
+		try:
+			newEvent.clean_fields()
+		except ValidationError as e:
+			print e
+			return Response({'success':False, 'error':e}, status=status.HTTP_400_BAD_REQUEST)
 
-        newEvent.save()
-        print 'New Event Logged from: ' + requestor
-        return Response({'success': True}, status=status.HTTP_200_OK)
+		newEvent.save()
+		print 'New Event Logged from: ' + requestor
+		return Response({'success': True}, status=status.HTTP_200_OK)
 
 
 class ActivateIFTTT(APIView):
-    permission_classes = (AllowAny,)
-    parser_classes = (parsers.JSONParser,parsers.FormParser)
-    renderer_classes = (renderers.JSONRenderer, )
+	permission_classes = (AllowAny,)
+	parser_classes = (parsers.JSONParser,parsers.FormParser)
+	renderer_classes = (renderers.JSONRenderer, )
 
-    def post(self,request):
-        print 'REQUEST DATA'
-        print str(request.data)
+	def post(self,request):
+		print 'REQUEST DATA'
+		print str(request.data)
 
-        eventtype = request.data.get('eventtype')
-        timestamp = int(request.data.get('timestamp'))
-        requestor = request.META['REMOTE_ADDR']
-        api_key = ApiKey.objects.all().first()
-        event_hook = "test"
+		eventtype = request.data.get('eventtype')
+		timestamp = int(request.data.get('timestamp'))
+		requestor = request.META['REMOTE_ADDR']
+		api_key = ApiKey.objects.all().first()
+		event_hook = "test"
 
-        print "Creating New event"
+		print "Creating New event"
 
-        newEvent = Event(
-            eventtype=eventtype,
-            timestamp=datetime.datetime.fromtimestamp(timestamp/1000, pytz.utc),
-            userid=str(api_key.owner),
-            requestor=requestor
-        )
+		newEvent = Event(
+			eventtype=eventtype,
+			timestamp=datetime.datetime.fromtimestamp(timestamp/1000, pytz.utc),
+			userid=str(api_key.owner),
+			requestor=requestor
+		)
 
-        print newEvent
-        print "Sending Device Event to IFTTT hook: " + str(event_hook)
+		print newEvent
+		print "Sending Device Event to IFTTT hook: " + str(event_hook)
 
-        #send the new event to IFTTT and print the result
-        event_req = requests.post('https://maker.ifttt.com/trigger/'+str(event_hook)+'/with/key/'+api_key.key, data= {
-            'value1' : timestamp,
-            'value2':  "\""+str(eventtype)+"\"",
-            'value3' : "\""+str(requestor)+"\""
-        })
-        print event_req.text
+		#send the new event to IFTTT and print the result
+		event_req = requests.post('https://maker.ifttt.com/trigger/'+str(event_hook)+'/with/key/'+api_key.key, data= {
+			'value1' : timestamp,
+			'value2':  "\""+str(eventtype)+"\"",
+			'value3' : "\""+str(requestor)+"\""
+		})
+		print event_req.text
 
-        #check that the event is safe to store in the databse
-        try:
-            newEvent.clean_fields()
-        except ValidationError as e:
-            print e
-            return Response({'success':False, 'error':e}, status=status.HTTP_400_BAD_REQUEST)
+		#check that the event is safe to store in the databse
+		try:
+			newEvent.clean_fields()
+		except ValidationError as e:
+			print e
+			return Response({'success':False, 'error':e}, status=status.HTTP_400_BAD_REQUEST)
 
-        #log the event in the DB
-        newEvent.save()
-        print 'New Event Logged'
-        return Response({'success': True}, status=status.HTTP_200_OK)
+		#log the event in the DB
+		newEvent.save()
+		print 'New Event Logged'
+		return Response({'success': True}, status=status.HTTP_200_OK)
