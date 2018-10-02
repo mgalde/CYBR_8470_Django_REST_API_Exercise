db3397c292625a01da37feda2fffc5386c20424d
Michael <30883926+mgalde@users.noreply.github.com>
2018-10-02 12:29:08 -0500

    Revert

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,9 +6,6 @@ from rest_framework import routers
 from api import controllers
 from django.views.decorators.csrf import csrf_exempt
 
-#More Imports
-from rest_framework.urlpatterns import format_suffix_patterns
-from snippets import views
 
 #REST API routes
 router = routers.DefaultRouter(trailing_slash=False)
@@ -20,6 +17,4 @@ urlpatterns = [
     url(r'^dogs', csrf_exempt(controllers.Events.as_view())),
     url(r'^activateifttt', csrf_exempt(controllers.ActivateIFTTT.as_view())),
     url(r'^', include(router.urls)),
-    url(r'^snippets/$', views.SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
