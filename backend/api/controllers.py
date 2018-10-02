c0c98ceff251d8bc39a73a245288fe95deb8afdd
Michael <30883926+mgalde@users.noreply.github.com>
2018-10-02 14:28:02 -0500

    It works, added 404 now

--- backend/api/controllers.py
+++ backend/api/controllers.py
@@ -1,5 +1,6 @@
 #from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
 # Create your views here.
